a121504ac2b81ef6da7c5a10bd05c987b0d5b92a
Yiping Wang <yiping.wang@gmx.com>
2020-08-03 16:15:12 -0700

    open timeline by default

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
    $('.collapsible').collapsible();
    $('.scrollspy').scrollSpy();
+   // open timeline by default
+   $('#timeline-collapsible').collapsible('open', 0);
  });
